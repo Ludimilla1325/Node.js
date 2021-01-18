0594941f0890ab75302bcebec5a5f31e69ccc964
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-01-18 10:31:28 -0300

    Socket.io built in events

--- 13/server/server.js
+++ 13/server/server.js
@@ -8,6 +8,16 @@ const io = socketIO(server);
 
 app.use(express.static(__dirname + '/../public'));
 
+//on- gonna be listening for something, 1st event is connection, receiving a connection
+//In this case we r listening for connections
+io.on('connection',(socket)=>{
+    console.log('someone is connected');
+
+    socket.on('disconnect',()=>{
+        console.log('User disconnected from server')
+    })
+})
+
 const port = process.env.PORT || 3004;
 server.listen(port,()=>{
     console.log(`Server running on port ${port}`)
