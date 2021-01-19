073457aa6ef37d4f28cea8e56b54f6404b7090ff
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-01-19 00:27:31 -0300

    Extending EventEmitter Class

--- 14_YT_Mosh/app.js
+++ 14_YT_Mosh/app.js
@@ -1,15 +1,11 @@
 const EventEmitter = require('events');
 
-const emitter = new EventEmitter
+const   Logger = require('./logger');
+const logger = new Logger();
 
 //Register a listener
-emitter.on('messageLogged',(arg)=>{
+logger.on('messageLogged',(arg)=>{
     console.log('Listener called', arg);
 });
 
-//Emit - making a noise, produce signal
-//Raise an event
-emitter.emit('messageLogged',{
-    id:1,
-    url:'http://'
-});
+logger.log('message');
