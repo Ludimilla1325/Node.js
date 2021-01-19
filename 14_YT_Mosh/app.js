9d674b7a36f5b0d715c511df6b4607fba3408def
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-01-18 23:32:24 -0300

    File System Module- readdirSync

--- 14_YT_Mosh/app.js
+++ 14_YT_Mosh/app.js
@@ -1,7 +1,4 @@
-const os = require ('os');
+const fs = require('fs');
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
-
-console.log(`Total memory: ${totalMemory}`);
-console.log(`Free memory: ${freeMemory}`);
+const files = fs.readdirSync('./')
+console.log(files);
