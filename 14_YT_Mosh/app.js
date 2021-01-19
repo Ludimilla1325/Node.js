8b338e02e2277d07b2f10e538c1718d1dbaf3273
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-01-18 23:55:36 -0300

    Event arguments

--- 14_YT_Mosh/app.js
+++ 14_YT_Mosh/app.js
@@ -3,10 +3,13 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter
 
 //Register a listener
-emitter.on('messageLogged', function(){
-    console.log('Listener called');
+emitter.on('messageLogged',(arg)=>{
+    console.log('Listener called', arg);
 });
 
 //Emit - making a noise, produce signal
 //Raise an event
-emitter.emit('messageLogged');
+emitter.emit('messageLogged',{
+    id:1,
+    url:'http://'
+});
